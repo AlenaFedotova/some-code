63ba04774c7179be6c4eeebfbf51a339c78a577a
AlenaFedotova <nyonk@mail.ru>
2017-08-08 21:39:02 +0400

    Update gotask.pl

--- perl/gotask.pl
+++ perl/gotask.pl
@@ -1,1 +1,14 @@
+use strict;
+use warnings;
+use feature 'say';
 
+my $file = shift;
+unless ($file =~ /\.\/.*/) {$file = "./$file"}
+
+open my $fh, "<", "$file" or die "Trouble with file: $!";
+
+my $num;
+while (<$fh>) {$num++}
+
+close $fh;
+say $num;
